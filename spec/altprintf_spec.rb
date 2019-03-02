27320bf2cd879b02a39d133e09dfb89ff3ff1aba
Stone Tickle <lattis@mochiro.moe>
2019-03-01 21:49:26 -0600

    Add some more specs

--- spec/altprintf_spec.rb
+++ spec/altprintf_spec.rb
@@ -8,4 +8,20 @@ RSpec.describe('altprintf') do
   it 'can insert a string' do
     expect(altprintf('test %s', 80, 'string')).to eq('test string')
   end
+
+  it 'can format doubles' do
+    expect(altprintf('%f', 80, 1.5)).to eq('1.500000')
+  end
+
+  it 'can format ints' do
+    expect(altprintf('%d', 80, 80)).to eq('80')
+  end
+
+  it 'can format chars' do
+    expect(altprintf('%c', 80, 'c')).to eq('c')
+  end
+
+  it 'can format *' do
+    expect(altprintf('%~#*', 80, 10)).to eq('#' * 10)
+  end
 end
