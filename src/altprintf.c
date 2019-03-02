f2347519e737ce9b20b98611527f0c451c5deb39
Stone Tickle <lattis@mochiro.moe>
2019-03-01 21:50:47 -0600

    Add new specifiers and add intermediate methods for old ones

--- src/altprintf.c
+++ src/altprintf.c
@@ -13,10 +13,48 @@ void default_format(struct format *f) {
   f->le = NULL;
 }
 
-void format(struct strbuf *sb, struct format *f, void (*to_s)(struct strbuf *, void *))
+void format_mul(struct strbuf *sb, struct format *f)
+{
+  long int *i = f->le->data;
+  strbuf_pad(sb, f->chararg, *i);
+}
+void format_tern(struct strbuf *sb, struct format *f)
+{
+  long int *b = f->le->data;
+  int first_half = 1;
+  wchar_t sep = f->chararg;
+  wchar_t *p = f->stringarg_start;
+  for (;p<f->stringarg_end;p++) {
+    LOG("*p: %lc, first half? %d, bool: %ld, sep: %lc\n", (wint_t)*p, first_half, *b, (wint_t)sep);
+    if (*p == sep) first_half = 0;
+    else if (*b && first_half) strbuf_append_char(sb, p);
+    else if (!*b && !first_half) strbuf_append_char(sb, p);
+  }
+}
+
+void format_string(struct strbuf *sb, struct format *f)
+{
+  int prec = f->width.prec == -1 ? 100000000 : f->width.prec;
+  strbuf_append_str(sb, f->le->data, prec);
+}
+void format_char(struct strbuf *sb, struct format *f)
+{
+  strbuf_append_char(sb, f->le->data);
+}
+void format_int(struct strbuf *sb, struct format *f)
+{
+  strbuf_append_int(sb, f->le->data);
+}
+void format_double(struct strbuf *sb, struct format *f)
+{
+  int prec = f->width.prec == -1 ? 3 : f->width.prec;
+  strbuf_append_double(sb, f->le->data, prec);
+}
+
+void format(struct strbuf *sb, struct format *f, void (*to_s)(struct strbuf *, struct format *))
 {
   struct strbuf *tmp = strbuf_new();
-  to_s(tmp, f->le->data);
+  to_s(tmp, f);
 
   int pad = f->width.pad - tmp->width;
 
@@ -50,7 +88,7 @@ wchar_t *altsprintf(wchar_t *fmt, struct list_elem *le, int final_width) {
   wchar_t *end = &fmt[wcslen(fmt)];
   wchar_t *jump;
 
-  void (*append_func)(struct strbuf *, void *);
+  void (*append_func)(struct strbuf *, struct format *);
   struct format f;
   long int *number_p = NULL;
 
@@ -68,11 +106,12 @@ wchar_t *altsprintf(wchar_t *fmt, struct list_elem *le, int final_width) {
         switch(*fmt) {
           /* special arguments */
           case FS_A_STRINGSTART:
-            f.stringarg_start = fmt;
+            f.stringarg_start = fmt + 1;
             lvl = 2;
             break;
           case FS_A_CHARARG:
             f.chararg = *(fmt+1);
+            fmt += 1;
             break;
 
           /* standard arguments */
@@ -87,6 +126,7 @@ wchar_t *altsprintf(wchar_t *fmt, struct list_elem *le, int final_width) {
             break;
           case '.':
             number_p = &f.width.prec;
+            fmt++;
           case '1': case '2': case '3': case '4': case '5':
           case '6': case '7': case '8': case '9':
             if (number_p == NULL) number_p = &f.width.pad;
@@ -96,16 +136,22 @@ wchar_t *altsprintf(wchar_t *fmt, struct list_elem *le, int final_width) {
 
           /* types */
           case FS_T_STRING:
-            append_func = strbuf_append_str;
+            append_func = format_string;
+            goto match;
+          case FS_T_TERN:
+            append_func = format_tern;
             goto match;
           case FS_T_INT:
-            append_func = strbuf_append_int;
+            append_func = format_int;
+            goto match;
+          case FS_T_MUL:
+            append_func = format_mul;
             goto match;
           case FS_T_CHAR:
-            append_func = strbuf_append_char;
+            append_func = format_char;
             goto match;
           case FS_T_DOUBLE:
-            append_func = strbuf_append_double;
+            append_func = format_double;
           match:
             if (le != NULL && le->type != Null) {
               f.le = le;
@@ -121,7 +167,8 @@ wchar_t *altsprintf(wchar_t *fmt, struct list_elem *le, int final_width) {
         }; break;
       case 2:
         if (*fmt == FS_A_STRINGEND) {
-          lvl = 2;
+          f.stringarg_end = fmt - 1;
+          lvl = 1;
         }; break;
     }
   }
