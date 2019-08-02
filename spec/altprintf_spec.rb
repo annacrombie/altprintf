a2338d2a38fe5418972a45c41afcdd167963a6de
Stone Tickle <lattis@mochiro.moe>
2019-08-02 16:02:46 -0500

    replace it blocks with compact testcase hash

--- spec/altprintf_spec.rb
+++ spec/altprintf_spec.rb
@@ -3,36 +3,20 @@ require_relative 'spec_helper'
 RSpec.describe('altprintf') do
   Backends.each do |backend|
     context backend.class.name do
-      it 'can echo' do
-        expect(backend.format('test')).to eq('test')
-      end
-
-      it 'can insert a string' do
-        expect(backend.format('test %s', 'string')).to eq('test string')
-      end
-
-      it 'can format doubles' do
-        expect(backend.format('%.3f', 1.5)).to eq('1.500')
-      end
-
-      it 'can format ints' do
-        expect(backend.format('%d', 80)).to eq('80')
-      end
-
-      it 'can format chars' do
-        expect(backend.format('%c', 'c')).to eq('c')
-      end
-
-      it 'can format *' do
-        expect(backend.format('%~#*', 10)).to eq('#' * 10)
-      end
-
-      it 'can format ?' do
-        expect(backend.format('%(a:b)?', 1)).to eq('a')
-      end
-
-      it 'can format =' do
-        expect(backend.format('a%~ =b', 10)).to eq('a        b')
+      {
+        ['test'] => 'test',
+        ['test %s', 'string'] => 'test string',
+        ['%.3f', 1.5] => '1.500',
+        ['%d', 80] => '80',
+        ['%c', 'c'] => 'c',
+        ['%~#*', 10] => '#' * 10,
+        ['%(a:b)?', 1] => 'a',
+        ['a%~ =b', 10] => 'a        b',
+        ['片%s', '仮名'] => '片仮名'
+      }.each do |args, result|
+        it "can format #{args}" do
+          expect(backend.format(*args)).to eq(result)
+        end
       end
     end
   end
