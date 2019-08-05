906d2a10b153574f831c67f9a27baa55c14f4cbe
Stone Tickle <lattis@mochiro.moe>
2019-08-05 06:24:01 -0700

    make test output prettier

--- spec/altprintf_spec.rb
+++ spec/altprintf_spec.rb
@@ -2,11 +2,13 @@ require_relative 'spec_helper'
 
 RSpec.describe('altprintf') do
   APIs.each do |api|
-    context api do
+    context api.class do
       Tests.for(api) do |group, tests|
         context group do
           tests.each do |args, result|
-            it "#{args[0]} \"#{args[1]}\" (#{args[2..].join(', ')}) => \"#{result}\"" do
+            pretty = args[1..].map(&:inspect).join(', ')
+
+            it "can #{args[0]}(#{pretty}) => \"#{result}\"" do
               expect(api.send(args[0], args[1..])).to eq(result)
             end
           end
