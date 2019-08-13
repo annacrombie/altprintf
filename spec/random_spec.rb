bf5306f5fafcae77cfdf7c40abaedd21172d061e
Stone Tickle <lattis@mochiro.moe>
2019-08-13 09:01:22 +0900

    append to log

--- spec/random_spec.rb
+++ spec/random_spec.rb
@@ -1,15 +1,23 @@
 require_relative 'spec_helper'
 require 'alt_printf'
 
-LOG_DIR = File.join(__dir__, '../log/')
+LOGS = File.join(__dir__, '../log/').then do |d|
+  {
+    out: 'random_spec.log',
+    err: 'random_spec_err.log'
+  }.map { |k, v| [k, File.join(d, v)] }.to_h
+end
 
 RSpec.describe 'random formats' do
   it do
     cnt = { ok: 0, err: 0, crash: 0, timeout: 0 }
     tests = 100
 
+    LOGS.each { |_, f| File.write(f, '') }
+
     tests.times do |i|
       fmt, args = Generator.fmt_with_args
+
       printf(
         "\r%14.14s %d/%d, [%s]\e[K",
         fmt,
@@ -20,10 +28,17 @@ RSpec.describe 'random formats' do
 
       pid =
         fork do
-          $stdout.reopen(File.join(LOG_DIR, 'random_spec.log'))
-          $stderr.reopen(File.join(LOG_DIR, 'random_spec.err.log'))
-          p fmt
-          p args
+          $stdout.reopen(LOGS[:out], 'a')
+          $stderr.reopen(LOGS[:err], 'a')
+          [$stdout, $stderr].each do |o|
+            o.printf(
+              "random_spec test %d\nfmt: %s\n args: %s\n-----\n",
+              i,
+              fmt,
+              args
+            )
+            o.flush
+          end
 
           AltPrintf.fmt(fmt, *args)
         end
@@ -58,6 +73,8 @@ RSpec.describe 'random formats' do
         %i[crash err timeout].detect { |k| cnt[k].positive? }
           .tap { |k| k.nil? ? false : puts("\nfailing-fast: #{k}") }
     end
+
+    puts
 
     expect(cnt[:ok]).to eq(tests)
   end
