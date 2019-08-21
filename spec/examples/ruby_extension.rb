94f4d232e16d21dd543a96c90d287d8fda3572ae
Stone Tickle <lattis@mochiro.moe>
2019-08-21 17:12:31 +0900

    specify : as the separator for the ? operator

--- spec/examples/ruby_extension.rb
+++ spec/examples/ruby_extension.rb
@@ -1,5 +1,5 @@
 group APIs::RubyExtension, {
-  [:fmtm, 3, "%%(%%%s:%%%s)?", 'd', '~**', false, 15] => '*' * 15,
+  [:fmtm, 3, "%%~:(%%%s:%%%s)?", 'd', '~**', false, 15] => '*' * 15,
   [:fmt, '%<age>d years old', { age: 25 }] => '25 years old',
   [:fmtm, 0, "%%"] => '%%',
 }
