5089aeb284550e5571ff136c710d88db5f131afa
Stone Tickle <lattis@mochiro.moe>
2019-08-23 22:03:39 +0900

    add a basic benchmark of gem against ruby printf

---
+++ spec/benchmark.rb
@@ -0,0 +1,17 @@
+require 'bundler/setup'
+require 'benchmark/ips'
+require_relative 'support/alt_printf'
+
+fmt_string = "hello %-20s | %3d | %.50f\n"
+args = ['Enots Elkcit', 42, Math::PI]
+
+kwfmt_string = "hello %<name>-20s | %3<meaning>d | %<pi>.50f\n"
+kwargs = { meaning: 42, pi: Math::PI, name: 'Enots Elkcit' }
+
+Benchmark.ips do |bm|
+  bm.report('sprintf') { sprintf(fmt_string, *args) }
+  bm.report('alt_printf') { AltPrintf.fmt(fmt_string, *args) }
+  bm.report('sprintf - kwargs') { sprintf(kwfmt_string, **kwargs) }
+  bm.report('alt_printf - kwargs') { AltPrintf.fmt(kwfmt_string, **kwargs) }
+  bm.compare!
+end
