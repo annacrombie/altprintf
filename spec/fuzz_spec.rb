7c8c638a2c082c66d55d14918e5322b00a567cc0
Stone Tickle <lattis@mochiro.moe>
2019-08-23 13:48:56 +0900

    add a fuzz test for invalid formats

--- spec/fuzz_spec.rb
+++ spec/fuzz_spec.rb
@@ -15,6 +15,26 @@ RSpec.describe 'fuzz test' do
       end
 
       api.fmt([fmt] + args)
-    end.yield_self { |res| expect(res[:ok]).to eq(100) }
+    end.yield_self { |res| expect(res[:ok]).to eq(times.count) }
+  end
+
+  it 'invalid formats (some errors rescued)' do
+    r = Runner.new('invalid_formats')
+    api = APIs::RubyExtension.new
+
+    r.run(times) do |id|
+      fmt, args = FmtGenerator.fmt_with_args
+      fmt = fmt.chars.shuffle.join
+
+      [$stdout, $stderr].each do |o|
+        o.printf("---\ntest #%d\nfmt: %s\nargs: %s\n", id, fmt, args)
+      end
+
+      begin
+        api.fmt([fmt] + args)
+      rescue ArgumentError, TypeError => e
+        puts "rescued #{e}"
+      end
+    end.yield_self { |res| expect(res[:ok]).to eq(times.count) }
   end
 end
