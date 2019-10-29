0fa64d5615c3852e0e25adcc93fa6a28c7c05818
Stone Tickle <lattis@mochiro.moe>
2019-10-29 13:27:33 +0900

    release version 0.3.2

--- gem/lib/altprintf/version.rb
+++ gem/lib/altprintf/version.rb
@@ -1,3 +1,3 @@
 module Altprintf
-  VERSION ||= "0.3.1"
+  VERSION ||= "0.3.2"
 end
--- subprojects/libaltprintf/include/altprintf/altprintf.h
+++ subprojects/libaltprintf/include/altprintf/altprintf.h
@@ -1,6 +1,6 @@
 #ifndef ALTPRINTF_H_
 #define ALTPRINTF_H_
-#define ALTPRINTF_VERSION "0.3.1"
+#define ALTPRINTF_VERSION "0.3.2"
 
 #include <altprintf/fmt.h>
 #include <altprintf/fmte.h>
