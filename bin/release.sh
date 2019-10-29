0f8204a7930881d1890b95d88e9f133ae7694b1b
Stone Tickle <lattis@mochiro.moe>
2019-10-29 13:25:15 +0900

    ask permission for everything

--- bin/release.sh
+++ bin/release.sh
@@ -1,32 +1,39 @@
 #!/bin/sh -eu
 
 getpermission() {
+  local response
+
   echo "$@ [y/n]"
   read response
   [ "$response" = "y" ]
 }
 
-new_ver="$(cat .version)"
-echo "updating version strings to ${new_ver}"
+local new_ver="$(cat .version)"
 
-sed -i 's/\(ALTPRINTF_VERSION\) ".*"/\1 "'"${new_ver}"'"/g' \
-  subprojects/libaltprintf/include/altprintf/altprintf.h
+getpermission "bump version to '${new_ver}'" && {
+  sed -i 's/\(ALTPRINTF_VERSION\) ".*"/\1 "'"${new_ver}"'"/g' \
+    subprojects/libaltprintf/include/altprintf/altprintf.h
 
-sed -i 's/\(VERSION ||=\) ".*"/\1 "'"${new_ver}"'"/g' \
-  gem/lib/altprintf/version.rb
+  sed -i 's/\(VERSION ||=\) ".*"/\1 "'"${new_ver}"'"/g' \
+    gem/lib/altprintf/version.rb
 
-getpermission "commit?" && {
-  git commit -am "release version ${new_ver}"
+  git -P status -s
+  git -P diff --shortstat
 
-  getpermission "tag?" && {
-    git tag v"${new_ver}"
+  getpermission "commit?" && {
+    git commit -am "release version ${new_ver}"
 
-    getpermission "push" && {
-      git push --tags
+    getpermission "tag?" && {
+      git tag v"${new_ver}"
 
-      getpermission "publish gem?" && {
-        cd gem/
-        bundle exec rake gem:publish
+      getpermission "push?" && {
+        git push
+        git push --tags
+
+        getpermission "publish gem?" && {
+          cd gem/
+          bundle exec rake gem:publish
+        }
       }
     }
   }
